468c5977fa9a19925463f403ff85bbafbc355d6b
Berlin <tommyonik@gmail.com>
2025-02-25 18:27:23 +0100

    ?

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,20 +1,23 @@
 import { NextRequest } from 'next/server';
 import { getEventById, updateEvent, deleteEvent } from '@/server/controllers/eventController';
 
-interface Props {
-  params: {
-    id: string;
-  };
-}
-
-export async function GET(_request: NextRequest, { params }: Props) {
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   return getEventById(params.id);
 }
 
-export async function PUT(request: NextRequest, { params }: Props) {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   return updateEvent(request, params.id);
 }
 
-export async function DELETE(_request: NextRequest, { params }: Props) {
+export async function DELETE(
+  _request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   return deleteEvent(params.id);
 } 
