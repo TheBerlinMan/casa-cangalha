64829b4847cfd6e2a36bcef176f33f3303f5576b
Berlin <tommyonik@gmail.com>
2025-03-22 23:25:36 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,92 +1,72 @@
-'use client';
+"use client";
 
 import React from "react";
 import Link from "next/link";
-import { useLanguage } from "@/app/context/LanguageContext";
 import { usePathname } from "next/navigation";
+import { Mail, Phone, MapPin, Instagram } from "lucide-react";
 
 const Footer = () => {
-  const { language } = useLanguage();
   const path = usePathname();
   const adminPath = "/admin";
 
-  const content = {
-    en: {
-      description: "Casa Cangalha is a place where you can find the best of the best in the world of music.",
-      links: {
-        mission: "Mission",
-        about: "About",
-        contact: "Contact",
-        support: "Support",
-        admin: "Admin"
-      },
-      address: "1 Cangalha, 1000, Portugal",
-      email: "info@casa-cangalha.com",
-      phone: "+351 912 345 678",
-      social: {
-        follow: "Follow us on social media",
-        instagram: "Instagram"
-      },
-      rights: "All rights reserved © Casa Cangalha"
-    },
-    pt: {
-      description: "Casa Cangalha é um lugar onde você pode encontrar o melhor do melhor no mundo da música.",
-      links: {
-        mission: "Missão",
-        about: "Sobre",
-        contact: "Contato",
-        support: "Apoie",
-        admin: "Admin"
-      },
-      address: "Rua Cangalha, 1000, Portugal",
-      email: "info@casa-cangalha.com",
-      phone: "+351 912 345 678",
-      social: {
-        follow: "Siga-nos nas redes sociais",
-        instagram: "Instagram"
-      },
-      rights: "Todos os direitos reservados © Casa Cangalha"
-    }
-  };
-
-  const currentContent = content[language];
-
-  if (path.startsWith(adminPath)) return
+  if (path.startsWith(adminPath)) return;
 
   return (
     <div>
-      <hr className="border-t border-gray-300" />
-      <div className="flex flex-col gap-2 py-4 px-8">
-        <div className="flex justify-between items-start ">
-          <div className="flex flex-col gap-4">
-            <div>Casa Cangalha</div>
-            <p className="text-md max-w-sm">
-              {currentContent.description}
-            </p>
-            <div className="flex gap-4">
-              {/* <Link href="/">{currentContent.links.mission}</Link> */}
-              <Link href="/">{currentContent.links.about}</Link>
-              {/* <Link href="/">{currentContent.links.contact}</Link> */}
-              <Link href="/">{currentContent.links.support}</Link>
-              <Link href="/admin">{currentContent.links.admin}</Link>
-            </div>
+      <hr className="border-t border-gray-300 mb-2" />
+      <div className="flex gap-20 py-4 px-8 mr-16 mb-8">
+        <div>
+          <div className="font-bold mb-5 max-w-xs">Casa Cangalha</div>
+          <div className="max-w-xs text-sm text-gray-600 mb-6">
+            Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+            вэб-дизайне. Lorem Ipsum является стандартной
           </div>
-          <div className="flex flex-col gap-4">
-            <div>Casa Cangalha</div>
-            <div>{currentContent.address}</div>
-            <div>{currentContent.email}</div>
-            <div>{currentContent.phone}</div>
-            <div>
-              {currentContent.social.follow}
-              <div>{currentContent.social.instagram}</div>
+          <div className="max-w-xs text-sm text-gray-600">&copy; All rights reserved by Casa Cangalha, 2025</div>
+        </div>
+        <div>
+          <div>
+            <div className="font-bold mb-5 max-w-xs text-gray-600">Pages</div>
+            <div className="text-sm text-gray-500 flex flex-col gap-1.5 text-gray-600">
+              <div>Newsletter</div>
+              <div>Events</div>
+              <div>About</div>
+              <div>Support</div>
+              <div>Shop</div>
+              <div>Admin</div>
             </div>
           </div>
         </div>
         <div>
-          <p>{currentContent.rights}</p>
+          <div className="font-bold mb-5 max-w-xs text-gray-600">Support</div>
+          <div className="text-sm font-gray-300 flex flex-col gap-1.5 text-gray-600">
+            <div>Patreon</div>
+            <div>Arob.se</div>
+            <div>Buy us a coffee</div>
+          </div>
+        </div>
+        <div>
+          <div className="font-bold mb-5 max-w-xs text-gray-600">Contact</div>
+          <div className="text-sm font-gray-300 flex flex-col gap-2 text-gray-600">
+            <div className="flex gap-2 items-start">
+              <MapPin size={20} />
+              <div>Cangalha, Auiruoca, MG, Brazil</div>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Phone size={20} />
+              <div>+55 11 98765-4321</div>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Mail size={20} />
+              <div>Casa.cangalha@gmail.com</div>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Instagram size={20} />
+              <div>Casa.cangalha</div>
+            </div>
+          </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
